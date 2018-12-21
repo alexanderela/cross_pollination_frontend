424d8769e03bd38766b0826250e4a68ccdf6d6ef
Alexander Ela <ela.alexander@gmail.com>
2018-12-20 19:06:18 -0700

    Add passing tests to userReducer

--- src/reducers/test/userReducer.test.js
+++ src/reducers/test/userReducer.test.js
@@ -3,15 +3,35 @@ import { userReducer } from '../userReducer';
 describe('userReducer', () => {
   const defaultState = {
     id: null,
-    loggedIn: null
+    loggedIn: false
   }
 
-  it.skip('should return default state when no type is given', () => {
+  it('should return default state when no type is given', () => {
     const newState = userReducer(undefined, {});
     expect(newState).toEqual(defaultState);
   });
 
-  it.skip('should add user to state on successful login', () => {});
+  it('should add user to state on successful login', () => {
+    const mockAction = {
+      type: 'SUCCESSFUL_LOGIN',
+      id: 1
+    }
 
-  it.skip('should remove user from state on sign out', () => {});
-})
+    const expected = {
+      id: 1,
+      loggedIn: true
+    }
+    const newState = userReducer(undefined, mockAction);
+    expect(newState).toEqual(expected);
+  });
+
+  it('should remove user from state on sign out', () => {
+    const state = {
+      id: 1,
+      loggedIn: true
+    }
+    const mockAction = { type: 'SIGN_OUT' }
+    const newState = userReducer(state, mockAction);
+    expect(newState).toEqual(defaultState);
+  });
+});
---
+++ src/reducers/userReducer.js
@@ -0,0 +1,15 @@
+const defaultState = {
+	id: null,
+	loggedIn: false,
+}
+
+export const userReducer = (state = defaultState, action) => {
+	switch (action.type) {
+		case 'SUCCESSFUL_LOGIN':
+			return {id: action.id, loggedIn: true}
+		case 'SIGN_OUT':
+			return defaultState;
+		default:
+			return state;
+	}
+}
