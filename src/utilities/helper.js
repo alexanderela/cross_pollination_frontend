46aaf8ee1efc5050808792bd6a92e15f45a063c8
Tobin Nelson <tobin.j.n@gmail.com>
2018-12-23 09:57:45 -0700

    Create a helper file to check if a country has been used and to build the object that will be sent to global store

---
+++ src/utilities/helper.js
@@ -0,0 +1,65 @@
+import countryNames from './countryNames' 
+
+//Checking if a country is already used against the usedCountries array from the global store
+//invoked on fetchCorrectCountry
+export const checkCountry = (country, usedCountries) => {
+  if (usedCountries.includes(country)) {
+    return true
+  } else {
+    return false
+  }
+}
+
+//this function is building the object for the current question that will ultimately be sent to the global store. 
+export const buildQuestion = (correctCountry) => {
+  
+  const questionOptions = getRandomOptions()
+
+  checkOptions(correctCountry.name, questionOptions)
+  
+  const multipleChoices = [...questionOptions, correctCountry.name]
+  const flagOptions = shuffleMultipleChoice(multipleChoices)
+  //Adding the multiple coice array to the correctCountry object
+  correctCountry.multipleChoice = flagOptions
+  return correctCountry
+}
+
+//This function grabs three random countries
+const getRandomOptions = () => {
+  //update as number of countries in database increases
+  const optionA = countryNames[Math.floor(Math.random() * 149)]
+  const optionB = countryNames[Math.floor(Math.random() * 149)]
+  const optionC = countryNames[Math.floor(Math.random() * 149)]
+
+  if (optionA === optionB || optionA === optionC || optionB === optionC ){
+    getRandomOptions()
+  } else {
+    const countryOptionsPrelim = [optionA, optionB, optionC]
+    return countryOptionsPrelim
+  }
+}
+
+//This function checks if any of the 'wrong' options are the corrct answer
+const checkOptions = (countryName, questionOptions) => {
+  if(questionOptions.includes(countryName)){
+    getRandomOptions()
+  } else {
+    return
+  }
+}
+
+//I don't understand this code, but it works. What's going on with the assignment of m? What is m?
+const shuffleMultipleChoice = (array) => {
+  let m = array.length, t, i;
+
+  while (m) {
+    i = Math.floor(Math.random() * m--);
+    t = array[m];
+    array[m] = array[i];
+    array[i] = t;
+  }
+
+  return array;
+}
+
+
