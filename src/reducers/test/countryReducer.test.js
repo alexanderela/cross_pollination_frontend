5dc55ba748ce4b24c2c52110e68b0314632d292d
Alexander Ela <ela.alexander@gmail.com>
2018-12-23 12:33:52 -0800

    Add refactored passing tests for countryReducer

--- src/reducers/test/countryReducer.test.js
+++ src/reducers/test/countryReducer.test.js
@@ -1,21 +1,34 @@
 import { countryReducer } from '../countryReducer';
 
 describe('countryReducer', () => {
-  const defaultState = [];
+  const defaultState = {};
 
   it('should return default state when no type is given', () => {
-    const newState = countryReducer(undefined, []);
+    const newState = countryReducer(undefined, {});
     expect(newState).toEqual(defaultState);
   });
 
-  it('should set country names as new state', () => {
-    const mockCountryNames = ["Azerbaijan", "Mexico"];
+  it('should set current country as new state', () => {
+    const mockCountry = {
+                          "id": 79,
+                          "name": "Turkmenistan",
+                          "flag": "/images/flags/turkmenistan.png",
+                          "country_outline": "/images/outlines/turkmenistan.png",
+                          "created_at": "2018-12-23T16:48:16.005Z",
+                          "updated_at": "2018-12-23T16:48:16.005Z",
+                          "multipleChoice": [
+                            "France",
+                            "Nigeria",
+                            "Turkmenistan",
+                            "Japan"
+                          ]
+                        };
     const mockAction = {
-      type: 'SET_COUNTRY_NAMES',
-      countryNames: mockCountryNames
+      type: 'SET_CURRENT_COUNTRY',
+      country: mockCountry
     }
     const newState = countryReducer([], mockAction);
 
-    expect(newState).toEqual(mockCountryNames);
+    expect(newState).toEqual(mockCountry);
   });
 });
