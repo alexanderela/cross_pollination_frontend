b4fd8ab784f06a925a747f738c46cc4e9b762c0c
Tobin Nelson <tobin.j.n@gmail.com>
2018-12-23 10:01:43 -0700

    Changed naming conventions

--- src/utilities/API.js
+++ src/utilities/API.js
@@ -1,5 +1,6 @@
 export const fetchData = async (url) => {
   const response = await fetch(url);
-  const jsonData = await response.json();
-  return jsonData;
+  const data = await response.json();
+  return data;
+  //changed name from jsonData to data. The data is no longer json at this point .json() will parse it. It is usable, just wrapped in a promise.
 };
