6b26591598b9e0f95c39beae65bb5b9d290b17bd
Tobin Nelson <tobin.j.n@gmail.com>
2018-12-22 20:23:45 -0700

    Updated mock data to be accurate after data fixes

--- src/mockData/mockData.js
+++ src/mockData/mockData.js
@@ -1,50 +1,49 @@
-let mockData = [ 
-  { name: 'Mexico',
-   flag: './images/flags/Mexico.png',
-   outline: './images/outlines/Mexico.png',
-   questions:
-    [ 'Tabasco Hoy',
-      'El Financiero & The Oaxaca Times',
-      'Oaxaca,Durango,Nayarit' ] },
- { name: 'Hungary',
-   flag: './images/flags/Hungary.png',
-   outline: './images/outlines/Hungary.png',
-   questions:
-    [ 'Magyar Nemzet',
-      'Its first king is known in English as St. Stephen & locally as Szent Istvan',
-      'Nograd,Pest,Somogy' ] },
- { name: 'Vietnam',
-   flag: './images/flags/Vietnam.png',
-   outline: './images/outlines/Vietnam.png',
-   questions: [ 'Nhan Dan' ] },
- { name: 'Ireland',
-   flag: './images/flags/Ireland.png',
-   outline: './images/outlines/Ireland.png',
-   questions:
-    [ 'The Connaught Telegraph & The Galway Advertiser',
-      'In this country\'s parliament, the Dail has 166 seats, the Seanad, 60' ] },
- { name: 'Sweden',
-   flag: './images/flags/Sweden.png',
-   outline: './images/outlines/Sweden.png',
-   questions:
-    [ 'Svenska Dagbladet & Sundsvalls Tidning',
-      'Ostergotland,Vasterbotten,Uppsala' ] },
- { name: 'South Africa',
-   flag: './images/flags/South Africa.png',
-   outline: './images/outlines/South Africa.png',
-   questions:
-    [ 'The Cape Business News & The Natal Witness',
-      'This country\'s post-apartheid national anthem is "Nkosi Sikelel\' Iafrka"',
-      'Prince Edward & Marion, 2 small islands of this country, lie about 1200 miles southeast of Cape Town',
-      'Namaqualand,Bushmanland,Zululand' ] },
- { name: 'Australia',
-   flag: './images/flags/Australia.png',
-   outline: './images/outlines/Australia.png',
-   questions:
-    [ 'The Warrnambool Standard & The Alice Springs News',
-      'It takes up an entire continent',
-      'The Bunyip &The Alice Springs News',
-      'Western Plateau,Great Victoria Desert,Great Artesian Basin' ] }
+let mockData = [ { name: 'Mexico',
+    flag: '/images/flags/mexico.png',
+    outline: '/images/outlines/mexico.png',
+    questions: 
+     [ 'Tabasco Hoy',
+       'El Financiero & The Oaxaca Times',
+       'Oaxaca,Durango,Nayarit' ] },
+  { name: 'Hungary',
+    flag: '/images/flags/hungary.png',
+    outline: '/images/outlines/hungary.png',
+    questions: 
+     [ 'Magyar Nemzet',
+       'Its first king is known in English as St. Stephen & locally as Szent Istvan',
+       'Nograd,Pest,Somogy' ] },
+  { name: 'Vietnam',
+    flag: '/images/flags/vietnam.png',
+    outline: '/images/outlines/vietnam.png',
+    questions: [ 'Nhan Dan' ] },
+  { name: 'Ireland',
+    flag: '/images/flags/ireland.png',
+    outline: '/images/outlines/ireland.png',
+    questions: 
+     [ 'The Connaught Telegraph & The Galway Advertiser',
+       'In this country\'s parliament, the Dail has 166 seats, the Seanad, 60' ] },
+  { name: 'Sweden',
+    flag: '/images/flags/sweden.png',
+    outline: '/images/outlines/sweden.png',
+    questions: 
+     [ 'Svenska Dagbladet & Sundsvalls Tidning',
+       'Ostergotland,Vasterbotten,Uppsala' ] },
+  { name: 'South Africa',
+    flag: '/images/flags/south africa.png',
+    outline: '/images/outlines/south africa.png',
+    questions: 
+     [ 'The Cape Business News & The Natal Witness',
+       'This country\'s post-apartheid national anthem is "Nkosi Sikelel\' Iafrka"',
+       'Prince Edward & Marion, 2 small islands of this country, lie about 1200 miles southeast of Cape Town',
+       'Namaqualand,Bushmanland,Zululand' ] },
+  { name: 'Australia',
+    flag: '/images/flags/australia.png',
+    outline: '/images/outlines/australia.png',
+    questions: 
+     [ 'The Warrnambool Standard & The Alice Springs News',
+       'It takes up an entire continent',
+       'The Bunyip &The Alice Springs News',
+       'Western Plateau,Great Victoria Desert,Great Artesian Basin' ] }
 ]
 
 export default mockData;
