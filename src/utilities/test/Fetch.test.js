fe1d1f4125123a8124a2e9d1df8b577790d3b13a
Alexander Ela <ela.alexander@gmail.com>
2018-12-22 21:05:37 -0700

    Add passing tests for Fetch.fetchCorrectCountry and Fetch.fetchAllCountries

--- src/utilities/test/Fetch.test.js
+++ src/utilities/test/Fetch.test.js
@@ -3,31 +3,40 @@ import * as API from '../API';
 jest.mock('../API');
 
 describe('Fetch', () => {
-		describe('fetchCorrectCountry', () => {
-			let url;
-			let mockId;
+	describe('fetchCorrectCountry', () => {
+		let mockId;
+		let url;
 
-			beforeEach(() => {
-				
-			})
-			it.skip('should call fetchCountries with the correct URL', async () => {})
-			it.skip('should return a resolved object', async () => {})
-			})
+		beforeEach(() => {
+			mockId = 90;
+			url = `https://flagz4u.herokuapp.com/api/v1/country/${mockId}`
+		})
 
-	  describe('fetchAllCountries', () => {
-	 		let url;
-	     beforeEach(() => {
-					url = 'https://flagz4u.herokuapp.com/api/v1/country';
-	     })
+		it('should call fetchCorrectCountry with the correct URL', async () => {
+			Fetch.fetchCorrectCountry(mockId);
+			expect(API.fetchData).toHaveBeenCalledWith(url);
+		})
 
-	     it('should call fetchCountries with the correct URL', async () => {
-	     		Fetch.fetchAllCountries()
-	     		expect(API.fetchData).toHaveBeenCalledWith(url)
-	     })
+		it('should return a resolved array', async () => {
+			const mockCorrectCountry = await Fetch.fetchCorrectCountry(mockId);
+			expect(mockCorrectCountry.results).toEqual([])
+		})
+	})
 
-	     it('should return a resolved array', async () => {
-	     		const countries = await Fetch.fetchAllCountries();
-	     		expect(countries.results).toEqual([])
+  describe('fetchAllCountries', () => {
+ 		let url;
+     beforeEach(() => {
+				url = 'https://flagz4u.herokuapp.com/api/v1/country';
+     })
+
+     it('should call fetchCountries with the correct URL', async () => {
+     		Fetch.fetchAllCountries();
+     		expect(API.fetchData).toHaveBeenCalledWith(url)
+     })
+
+     it('should return a resolved array', async () => {
+     		const countries = await Fetch.fetchAllCountries();
+     		expect(countries.results).toEqual([])
 	  })
 	})
 })
