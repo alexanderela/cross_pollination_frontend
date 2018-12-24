330cfc6ffc9f5650154687cc29f6c31a38c4c925
Tobin Nelson <tobin.j.n@gmail.com>
2018-12-24 06:28:27 -0700

    Test the fetch call for getting a single country

--- src/utilities/Fetch.js
+++ src/utilities/Fetch.js
@@ -5,12 +5,9 @@ export const fetchCorrectCountry = async (randomId, usedCountries) => {
   const url = `https://flagz4u.herokuapp.com/api/v1/country/${randomId}`;
 
   const countryArray = await API.fetchData(url);
-  //returns as a single object in array, gets rid of array here and pass object
   const correctCountry = countryArray[0]
-  //check if country has been used, if it has make fetch again
-  //not sure if this is working --> needs testing!!
+
   if (checkCountry(correctCountry, usedCountries)) {
-    //update range as countries are added
     let randomNumber = Math.floor(Math.random() * (149 - 1) + 1);
     fetchCorrectCountry(randomNumber)
   } else {
@@ -20,8 +17,8 @@ export const fetchCorrectCountry = async (randomId, usedCountries) => {
 
 
 //would this ever be used?
-export const fetchAllCountries = async () => {
-  const url = 'https://flagz4u.herokuapp.com/api/v1/country';
-  const allCountries = await API.fetchData(url);
-  return allCountries
-}
+// export const fetchAllCountries = async () => {
+//   const url = 'https://flagz4u.herokuapp.com/api/v1/country';
+//   const allCountries = await API.fetchData(url);
+//   return allCountries
+// }
--- src/utilities/test/Fetch.test.js
+++ src/utilities/test/Fetch.test.js
@@ -1,44 +1,111 @@
 import * as Fetch from '../Fetch';
 import * as API from '../API';
+import * as helper from '../helper';
 jest.mock('../API');
 
 describe('Fetch', () => {
 	describe('fetchCorrectCountry', () => {
 		let mockId;
 		let mockUsedCountries;
+    let mockResults;
+    let mockCountry;
+
 		let url;
 
 		beforeEach(() => {
-			mockId = 5;
+			mockId = 79;
 			mockUsedCountries = ['Sweden', 'Denmark', 'Nigeria', 'Australia']
 			url = `https://flagz4u.herokuapp.com/api/v1/country/${mockId}`
+      helper.checkCountry = jest.fn().mockImplementation(() => { return false})
+      helper.buildQuestion = jest.fn().mockImplementation(() => {
+        return mockCountry
+      })
+
+      mockResults = [{
+        "id": 79,
+        "name": "Turkmenistan",
+        "flag": "/images/flags/turkmenistan.png",
+        "country_outline": "/images/outlines/turkmenistan.png",
+        "created_at": "2018-12-23T16:48:16.005Z",
+        "updated_at": "2018-12-23T16:48:16.005Z",
+      }]
+
+      mockCountry = {
+        "id": 79,
+        "name": "Turkmenistan",
+        "flag": "/images/flags/turkmenistan.png",
+        "country_outline": "/images/outlines/turkmenistan.png",
+        "created_at": "2018-12-23T16:48:16.005Z",
+        "updated_at": "2018-12-23T16:48:16.005Z",
+        "multipleChoice": [
+          "France",
+          "Nigeria",
+          "Turkmenistan",
+          "Japan"
+        ]
+      };
+
+      API.fetchData = jest.fn()
+            .mockImplementation(() => {
+              return mockResults
+            }
+          );
+
 		})
 
-		it('should call fetchCorrectCountry with the correct URL', async () => {
+
+      const mockFetchCall = jest.fn().mockImplementation(() => {
+    return Promise.resolve(mockResponse);
+  });
+
+		it('should call fetchData with the correct URL', async () => {
 			Fetch.fetchCorrectCountry(mockId, mockUsedCountries);
 			expect(API.fetchData).toHaveBeenCalledWith(url);
 		})
 
-		it.skip('should return a resolved array', async () => {
-			const mockCorrectCountry = await Fetch.fetchCorrectCountry(mockId, mockUsedCountries);
-			expect(mockCorrectCountry).toEqual([])
-		})
+    it('fetchData should return an array', () => {
+      Fetch.fetchCorrectCountry(mockId, mockUsedCountries);
+      expect(API.fetchData).toHaveReturnedWith(mockResults);
+    })
+
+    it('should call checkCountry', () => {
+      const spy = jest.spyOn(helper, 'checkCountry');
+      const isCalled = helper.checkCountry();
+      Fetch.fetchCorrectCountry(mockId, mockUsedCountries);
+
+      expect(helper.checkCountry).toHaveBeenCalled()
+      expect(isCalled).toBe(false);
+
+      spy.mockRestore();
+    })
+
+    it('should call buildQuestion', () => {
+      const spy = jest.spyOn(helper, 'buildQuestion');
+      const isCalled = helper.buildQuestion();
+      Fetch.fetchCorrectCountry(mockId, mockUsedCountries);
+
+      expect(helper.buildQuestion).toHaveBeenCalled()
+      expect(isCalled).toBe(mockCountry);
+
+
+      spy.mockRestore();
+    })
 	})
 
-  describe('fetchAllCountries', () => {
- 		let url;
-     beforeEach(() => {
-				url = 'https://flagz4u.herokuapp.com/api/v1/country';
-     })
+ //  describe('fetchAllCountries', () => {
+ // 		let url;
+ //     beforeEach(() => {
+	// 			url = 'https://flagz4u.herokuapp.com/api/v1/country';
+ //     })
 
-     it('should call fetchCountries with the correct URL', async () => {
-     		Fetch.fetchAllCountries();
-     		expect(API.fetchData).toHaveBeenCalledWith(url)
-     })
+ //     it('should call fetchCountries with the correct URL', async () => {
+ //     		Fetch.fetchAllCountries();
+ //     		expect(API.fetchData).toHaveBeenCalledWith(url)
+ //     })
 
-     it('should return a resolved array', async () => {
-     		const countries = await Fetch.fetchAllCountries();
-     		expect(countries.results).toEqual([])
-	  })
-	})
+ //     it('should return a resolved array', async () => {
+ //     		const countries = await Fetch.fetchAllCountries();
+ //     		expect(countries.results).toEqual([])
+	//   })
+	// })
 })
