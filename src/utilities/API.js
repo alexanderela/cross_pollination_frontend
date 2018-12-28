1ea7fa4265ae30b99bd0d0d1c05eaefdfd9c56ba
bhayek12@gmail.com <bhayek12@gmail.com>
2018-12-28 12:39:57 -0700

    Create add user function in API

--- src/utilities/API.js
+++ src/utilities/API.js
@@ -4,3 +4,21 @@ export const fetchData = async (url) => {
   return data;
   //changed name from jsonData to data. The data is no longer json at this point .json() will parse it. It is usable, just wrapped in a promise.
 };
+
+export const addUser = async (name, email, password) => {
+  try {
+    const url = 'https://flagz4u.herokuapp.com/signup'
+    const response = await fetch(url, {
+      method: 'POST',
+      body: JSON.stringify({name: name, email: email, password: password}),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
+    const result = await response.json();
+    const user = {id: result.id}
+    return user
+  } catch(error) {
+    throw new Error()
+  }
+}
