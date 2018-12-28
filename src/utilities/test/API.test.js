756eeed6f1285c1a62e224613907c26e1165086e
bhayek12@gmail.com <bhayek12@gmail.com>
2018-12-28 13:43:22 -0700

    Add test for jsond user

--- src/utilities/test/API.test.js
+++ src/utilities/test/API.test.js
@@ -32,7 +32,8 @@ describe('API', () => {
      				{ data: { name: 'bob', email: 'email@email.com', password: 'password' }}
      			)
      		}))
-     })
+	 })
+	 
      it('should call fetch on path /api/signup', () => {
 		const url = 'https://flagz4u.herokuapp.com/signup';
 		const body = {"body": "{\"name\":\"https://flagz4u.herokuapp.com/signup\"}", "headers": {"Content-Type": "application/json"}, "method": "POST"}
@@ -40,8 +41,15 @@ describe('API', () => {
 		API.addUser(url, body);
 		expect(window.fetch).toHaveBeenCalledWith(url, expected);	 
 	 })
-     it.skip('should pass an options object with stringified user data to fetch', () => {})
-     it.skip('Should return json\'d response from response', () => {})
+
+	 it('Should return json\'d response from fetch', async () => {
+		const url = 'https://flagz4u.herokuapp.com/signup';
+		const expected = { data: mockUser }
+		const response = await API.fetchData(url)
+		expect(response).toEqual(expected);
+	});
+	
+	it.skip('should pass an options object with stringified user data to fetch', () => {})
  })
 
  describe('createUser', () => {
