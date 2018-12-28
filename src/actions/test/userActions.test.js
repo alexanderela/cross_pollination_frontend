a0702472ef06704be31c8faf8b31427b4bffd476
bhayek12@gmail.com <bhayek12@gmail.com>
2018-12-28 13:27:16 -0700

    Add test for content status action

--- src/actions/test/userActions.test.js
+++ src/actions/test/userActions.test.js
@@ -19,6 +19,17 @@ describe('userActions', () => {
 
       expect(result).toEqual(expected);
     });
+
+    it('should have a type of CONTENT_STATUS', () => {
+      const status = '';
+      const expected = {
+        type: 'CONTENT_STATUS',
+        status
+      }
+      const result = userActions.contentStatus(status);
+      expect(result).toEqual(expected);
+    });
+
   });
   describe('signOut', () => {
     it('should have a type of SIGN_OUT and userData', () => {
