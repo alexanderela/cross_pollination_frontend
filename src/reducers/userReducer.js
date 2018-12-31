5d96dcb000dbc7735497b4d5fe2d9cd5e0a62494
bhayek12@gmail.com <bhayek12@gmail.com>
2018-12-31 11:58:57 -0700

    Add id and user to user reducer

--- src/reducers/userReducer.js
+++ src/reducers/userReducer.js
@@ -1,12 +1,14 @@
 const defaultState = {
 	id: null,
 	loggedIn: false,
+	user: ''
 }
 
 export const userReducer = (state = defaultState, action) => {
+	console.log(action.user)
 	switch (action.type) {
 		case 'SUCCESSFUL_LOGIN':
-			return {id: action.id, loggedIn: true}
+			return {id: action.id, user: action.user, loggedIn: true}
 		case 'SIGN_OUT':
 			return defaultState;
 		default:
