57e128cd4ff01e171813756c7fb5eae919b72f13
bhayek12@gmail.com <bhayek12@gmail.com>
2019-01-02 14:47:28 -0700

    Add loading to store

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,9 +2,11 @@ import { combineReducers } from 'redux';
 import { countryReducer } from './countryReducer';
 import { userReducer } from './userReducer';
 import { usedCountriesReducer } from './usedCountriesReducer';
+import { loadingReducer } from './loadingReducer'
 
 export const rootReducer = combineReducers({
   currentCountry: countryReducer,
   usedCountries: usedCountriesReducer,
   user: userReducer,
+  loading: loadingReducer
 });
