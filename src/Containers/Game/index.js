2d54fafb2c3bdf17ff346b460159994727aea32d
Tobin-jn <tobin.j.n@gmail.com>
2019-01-03 13:39:53 -0700

    Decrement hints counter

--- src/Containers/Game/index.js
+++ src/Containers/Game/index.js
@@ -149,7 +149,7 @@ export class Game extends Component {
           <img src={flagImage} alt='' className='flag-image'/>
         </div>
         <div className='hint-button' onClick={this.giveHint}>
-          Hints: 2
+          Hints: {this.state.pointsPossible-1}
         </div>
         
         { choiceButtons }
