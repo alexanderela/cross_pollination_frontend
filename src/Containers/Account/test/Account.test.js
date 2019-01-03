d4b4feccb064b222e5b9a93a03240f473a6c2aec
bhayek12@gmail.com <bhayek12@gmail.com>
2019-01-03 10:33:59 -0700

    Update snapshot

--- src/Containers/Account/test/Account.test.js
+++ src/Containers/Account/test/Account.test.js
@@ -4,9 +4,12 @@ import Account from '../index';
 
 describe('Account', () => {
   let wrapper;
+  let mockUser;
+  let signOut;
 
   beforeEach(() => {
-    wrapper = shallow(<Account />)
+    mockUser = {name: 'doug' }
+    wrapper = shallow(<Account user={mockUser} signOut={jest.fn()}/>)
   });
 
   it('should render like the snapshot', () => {
