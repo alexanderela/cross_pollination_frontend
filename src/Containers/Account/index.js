c2247208a063dd8a3097b984d18689e75ca7c631
bhayek12@gmail.com <bhayek12@gmail.com>
2019-01-03 10:40:11 -0700

    Add proptypes

--- src/Containers/Account/index.js
+++ src/Containers/Account/index.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom';
 import { signOut } from '../../actions/userActions';
 import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 import './Account.scss';
 
 class Account extends Component {
