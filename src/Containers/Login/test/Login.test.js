270b4f460ab9fc5b2f206b4e74c264c2f291a6c9
Alexander Ela <ela.alexander@gmail.com>
2019-01-03 19:08:19 -0700

    Add test variable to login

--- src/Containers/Login/test/Login.test.js
+++ src/Containers/Login/test/Login.test.js
@@ -4,6 +4,7 @@ import { Login, mapStateToProps, mapDispatchToProps } from '../index';
 import { fetchUser } from '../../../Thunks/user'
 
 describe('Login', () => {
+  let alex;
   let wrapper;
   let mockEvent;
   let mockFunc;
