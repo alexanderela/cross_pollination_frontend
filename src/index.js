c0a20048ed60aeb627039680de5fe5df5f3b7f46
bhayek12@gmail.com <bhayek12@gmail.com>
2019-01-03 19:21:56 -0700

    Fix lints

--- src/index.js
+++ src/index.js
@@ -1,20 +1,21 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import { BrowserRouter } from 'react-router-dom'
-import './index.css'
-import App from './Containers/App'
-import * as serviceWorker from './serviceWorker'
-import { Provider } from 'react-redux'
-import { rootReducer } from './reducers'
-import { createStore, applyMiddleware } from 'redux'
-import { composeWithDevTools } from 'redux-devtools-extension'
-import thunk from 'redux-thunk'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import './index.css';
+import App from './Containers/App';
+import * as serviceWorker from './serviceWorker';
+import { Provider } from 'react-redux';
+import { rootReducer } from './reducers';
+import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import thunk from 'redux-thunk';
 
-// const devTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+// const devTools = window.__REDUX_DEVTOOLS_EXTENSION__ 
+// && window.__REDUX_DEVTOOLS_EXTENSION__();
 const store = createStore(
   rootReducer,
   composeWithDevTools(applyMiddleware(thunk))
-)
+);
 
 const router = (
   <Provider store={store}>
@@ -22,11 +23,11 @@ const router = (
       <App />
     </BrowserRouter>
   </Provider>
-)
+);
 
-ReactDOM.render(router, document.getElementById('root'))
+ReactDOM.render(router, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister()
+serviceWorker.unregister();
